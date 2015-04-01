684f580eae31824d1cf79f64b9f55fdeef195af4
Dylan Meissner <dylanmei@gmail.com>
2015-04-01 11:31:20 -0700

    Add reverse proxy

--- config/config.go
+++ config/config.go
@@ -6,16 +6,16 @@ import (
 )
 
 type Config struct {
-	
 	Host string `toml:"host"`
 	Port string `toml:"port"`
+	EtcdAddr string `toml:"etcd-addr"`
 }
 
 func New() *Config {
 	c := new(Config)
 	c.Host = "127.0.0.1"
 	c.Port = "8080"
-	
+	c.EtcdAddr = "localhost:4001"
 	return c
 }
 
@@ -25,6 +25,7 @@ func (c *Config) LoadFlags(arguments []string) error {
 	f := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
 	f.StringVar(&c.Host, "host", c.Host, "")
 	f.StringVar(&c.Port, "port", c.Port, "")
+	f.StringVar(&c.EtcdAddr, "etcd-addr", c.Port, "")
 	
 	if err := f.Parse(arguments); err != nil {
 		return err
--- main.go
+++ main.go
@@ -3,30 +3,32 @@ package main
 import (
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
-	"fmt"
-	
+
+	"github.com/AdoHe/etcd-dashboard/config"
+	"github.com/AdoHe/etcd-dashboard/dashboard"
 	"github.com/gorilla/mux"
-	"etcd-dashboard/dashboard"
-	"etcd-dashboard/config"
 )
 
 func main() {
-	
 	var config = config.New()
-	
 	if err := config.LoadFlags(os.Args[1:]); err != nil {
-		fmt.Println(err.Error() + "\n")
-		os.Exit(1)	
+		log.Fatal(err)
 	}
-	
+
 	r := mux.NewRouter()
 	r.PathPrefix("/mod").Handler(http.StripPrefix("/mod", dashboard.HTTPHandler()))
-	
+
+	target, _ := url.Parse("http://" + config.EtcdAddr)
+	p := httputil.NewSingleHostReverseProxy(target)
+	r.PathPrefix("/v2").Handler(p)
+
 	http.Handle("/", r)
 	var addr string = config.Host + ":" + config.Port
 	err := http.ListenAndServe(addr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
-	}								
+	}
 }
